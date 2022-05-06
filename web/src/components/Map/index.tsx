cde6eca67206fc857653c5f5b9e76afacfeb313a
Aleksi Leino <aleksi@leino.io>
2022-05-06 22:15:58 +0300

    feat(web): Update temperatures on map when the unit changes

--- web/src/components/Map/index.tsx
+++ web/src/components/Map/index.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
-import mapboxgl from "mapbox-gl";
+import mapboxgl, { GeoJSONSource } from "mapbox-gl";
 
 import { useStoreState } from "../../store";
+import { getMeasurementsWithUserPreferences } from "../../utils/measurements";
 import {
   initialCenter,
   initialZoom,
@@ -11,8 +12,19 @@ import {
 } from "./helpers";
 
 const Map = () => {
+  // Get and modify data shown on the map
   const measurements = useStoreState((state) => state.measurements.data);
-  const featureCollection = getFeatureCollection(measurements);
+  const temperatureUnit = useStoreState(
+    (state) => state.userPreferences.temperatureUnit
+  );
+  const measurementsWithUserPreferences = getMeasurementsWithUserPreferences(
+    measurements,
+    temperatureUnit
+  );
+
+  const featureCollection = getFeatureCollection(
+    measurementsWithUserPreferences
+  );
 
   const [ready, setReady] = useState(false);
   const mapContainer = React.useRef(null);
@@ -39,7 +51,10 @@ const Map = () => {
 
     const sourceName = "measurements";
     if (map.current.getSource(sourceName)) {
-      map.current.removeSource(sourceName);
+      (map.current.getSource(sourceName) as GeoJSONSource).setData(
+        featureCollection
+      );
+      return;
     }
 
     map.current.addSource(sourceName, {
---
+++ web/src/utils/index.ts
@@ -0,0 +1,3 @@
+export const celsiusToFahrenheit = (celsius: number): number => {
+  return celsius * 1.8 + 32;
+};
